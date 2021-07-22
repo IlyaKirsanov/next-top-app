f0ed0ec7267f4063dc702a2d29681abf755383d7
IlyaKirsanov <ilya.kirsanov@gmail.com>
2021-07-22 11:35:55 +0300

    Transfer head to app from index

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import { AppProps } from 'next/dist/next-server/lib/router/router';
 import '../styles/globals.css';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+	return (
+		<>
+			<Head>
+				<title>Next top app</title>
+				<link key={1} rel="icon" href="./favicon.ico" />
+			</Head>
+
+			<Component {...pageProps} />;
+		</>);
 }
 
 export default MyApp;
