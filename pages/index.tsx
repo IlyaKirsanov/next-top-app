d818a737e93dd5ea8ef661031b4f0b17eafdad3e
IlyaKirsanov <ilya.kirsanov@gmail.com>
2021-07-22 13:05:50 +0300

    Create Button with extended HTML element props !!!

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
-import { Htag } from '../components';
+import { Htag, Button} from '../components';
 
 
 export default function Home(): JSX.Element {
 	return (
 		<>
 			<Htag tag="h1">Текст</Htag>
+			<Button appearance="primary">Кнопка</Button>
+			<Button appearance="ghost">Кнопка2</Button>
 		</>
 	);
 }
