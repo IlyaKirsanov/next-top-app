958260b8eb2c9791152b96b3155a75a9f95399ce
IlyaKirsanov <ilya.kirsanov@gmail.com>
2021-07-28 21:31:15 +0300

    Test using getStaticProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,56 @@
-import { useState } from 'react';
-import { Htag, Button, P, Tag, Rating } from '../components';
-import { withLayout } from '../layout/Layout';
+import { useState } from 'react'
+import { Htag, Button, P, Tag, Rating } from '../components'
+import { withLayout } from '../layout/Layout'
+import { GetStaticProps } from 'next'
+import axios from 'axios'
+import { MenuItem } from '../interface/menu.interface'
 
 
-function Home(): JSX.Element {
-
-	const [rating, setRating] = useState<number>(4);
-
+function Home({ menu }: HomeProps): JSX.Element {
+	
+	const [ rating, setRating ] = useState<number>(4);
+	
 	return (
 		<>
-			<Htag tag="h1">{rating}</Htag>
+			<Htag tag="h1">{ rating }</Htag>
 			<Button appearance="primary" arrow="right">Кнопка</Button>
 			<Button appearance="ghost" arrow="down">Кнопка2</Button>
-			<P size="large">Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam porro soluta consectetur corporis magni qui incidunt impedit est officia sequi.</P>
-			<P size="medium">Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam porro soluta consectetur corporis magni qui incidunt impedit est officia sequi.</P>
-			<P size="small">Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam porro soluta consectetur corporis magni qui incidunt impedit est officia sequi.</P>
+			<P size="large">Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam porro soluta consectetur corporis
+				magni qui incidunt impedit est officia sequi.</P>
+			<P size="medium">Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam porro soluta consectetur
+				corporis
+				magni qui incidunt impedit est officia sequi.</P>
+			<P size="small">Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam porro soluta consectetur corporis
+				magni qui incidunt impedit est officia sequi.</P>
 			<Tag color="red" size="small" href="#">Medium</Tag>
 			<Tag color="primary" size="medium" href="#">Medium</Tag>
 			<Tag color="green" size="small" href="#">Medium</Tag>
-			<Rating rating={rating} isEditable setRating={setRating}/>
+			<Rating rating={ rating } isEditable setRating={ setRating }/>
+			<ul>
+				{ menu.map(m => {
+					return <li key={ m._id.secondCategory }>{ m._id.secondCategory }</li>
+				}) }
+			</ul>
 		</>
 	);
 }
 
-export default withLayout(Home)
+export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps = async () => {
+	const firstCategory = 0
+	const { data: menu } = await axios.post<MenuItem[]>(
+		process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find',
+		{firstCategory});
+	return {
+		props: {
+			menu,
+			firstCategory
+		}
+	}
+}
+
+interface HomeProps extends Record<string, unknown>{
+	menu: MenuItem[],
+	firstCategory: number
+}
