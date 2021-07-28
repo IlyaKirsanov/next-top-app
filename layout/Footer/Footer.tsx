3f732e09668da47b857edfe88c84cdb39131fd73
IlyaKirsanov <ilya.kirsanov@gmail.com>
2021-07-28 13:50:10 +0300

    minor rafactoring

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,14 +1,14 @@
-import { FooterProps } from './Footer.props';
-import styles from './Footer.module.css';
-import cn from 'classnames';
-import {format} from 'date-fns';
+import { FooterProps } from "./Footer.props";
+import styles from "./Footer.module.css";
+import cn from "classnames";
+import { format } from "date-fns";
 
-export const Footer = ({className, ...props }: FooterProps): JSX.Element => {
+export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
 
 	return (
 		<div {...props} className={cn(className, styles.footer)}>
 			<div>
-				OwlTop © 2020 - {format(new Date(), 'yyyy')} Все права защищены
+				OwlTop © 2020 - {format(new Date(), "yyyy")} Все права защищены
 			</div>
 			<a href="#" target="_blank">Пользовательское соглашение</a>
 			<a href="#" target="_blank">Политика конфиденциальности</a>
