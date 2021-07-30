10f7ab9d77b846e3f96eda8fd38905b3a44f4d87
IlyaKirsanov <ilya.kirsanov@gmail.com>
2021-07-30 12:24:09 +0300

    Restructure routing with dynamic paths

---
+++ pages/[type]/index.tsx
@@ -0,0 +1,60 @@
+import Link from 'next/link';
+import { withLayout } from '../../layout/Layout';
+import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from 'next';
+import { MenuItem } from '../../interface/menu.interface';
+import axios from 'axios';
+import { firstLevelMenu } from '../../helpers/helpers';
+import { ParsedUrlQuery } from 'querystring';
+
+function Type({firstCategory}: TypeProps): JSX.Element {
+	return (
+		<div>
+
+			<Link href="/">
+				<a>{firstCategory}</a>
+			</Link>
+
+		</div>
+	);
+}
+
+export default withLayout(Type);
+
+export const getStaticPaths: GetStaticPaths = async () => {
+	return {
+		paths: firstLevelMenu.map(menuItem => `/${menuItem.route}`),
+		fallback: true
+	};
+};
+
+
+export const getStaticProps: GetStaticProps = async ({ params }: GetStaticPropsContext<ParsedUrlQuery>) => {
+
+	if (!params) {
+		return {
+			notFound: true
+		}
+	}
+
+	const firstCategoryItem = firstLevelMenu.find(m => m.route === params.type);
+
+	if (!firstCategoryItem) {
+		return {
+			notFound: true
+		};
+	}
+
+	const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', { firstCategory: firstCategoryItem.id });
+
+	return {
+		props: {
+			firstCategory: firstCategoryItem.id,
+			menu
+		}
+	};
+};
+
+interface TypeProps extends Record<string, unknown> {
+	menu: MenuItem[],
+	firstCategory: number
+}
